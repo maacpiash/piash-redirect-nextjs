9533570804a5b2c5b3a106166a8cc9a49e4aa303
Ahad Chowdhury <ahad@maacpiash.com>
2021-07-04 11:40:55 +1000

    ✨ Add hard-coded redirection for GitHub repos

--- next.config.js
+++ next.config.js
@@ -6,7 +6,13 @@ module.exports = {
         source: '/:ShortKey',
         destination: '/api/redirects/:ShortKey',
         permanent: true,
-      }
+      },
+      {
+        source: '/gh/:repo',
+        destination: 'https://github.com/maacpiash/:repo',
+        permanent: true,
+        basePath: false,
+      },
     ]
   }
 }
